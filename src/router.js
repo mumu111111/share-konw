0147d1383c63f5ab3c4d40334c829d8c0f349e00
mumu111111 <2646709035@qq.com>
2019-09-04 04:52:43 +0800

    '路由配置'

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,16 +7,35 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: () => import('./pages/Index/template.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/my',
+      component: () => import('./pages/My/template.vue')
+    },
+    {
+      path: '/user/:userId',
+      component: () => import('./pages/User/template.vue')
+    },
+    {
+      path: '/detail/:blogId',
+      component: () => import('./pages/Detail/template.vue')
+    },
+    {
+      path: '/login',
+      component: () => import('./pages/Login/template.vue')
+    },
+    {
+      path: '/register',
+      component: () => import('./pages/Register/template.vue')
+    },
+    {
+      path: '/edit/:blogId',
+      component: () => import('./pages/Edit/template.vue')
+    },
+    {
+      path: '/create',
+      component: () => import('./pages/Create/template.vue')
     }
   ]
 })
