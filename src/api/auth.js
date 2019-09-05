8aebd2ba597b80bc014e88723ceca5507727d45e
mumu111111 <2646709035@qq.com>
2019-09-05 09:38:18 +0800

    '封装api接口'

---
+++ src/api/auth.js
@@ -0,0 +1,27 @@
+import request from '../helper/request.js'
+const URLLIST = {
+    REGISTER: '/auth/register',
+    LOGIN: '/auth/login',
+    LOGOUT: '/auth/logout',
+    LOGINFO: '/auth'
+}
+
+export default {
+    //注册
+    register({ username, password }) {
+        return request(URLLIST.REGISTER, 'POST', { username, password })
+    },
+    //登录
+    login({ username, password }) {
+        return request(URLLIST.LOGIN, 'POST', { username, password })
+    },
+    //登出
+    logout() {
+        return request(URLLIST.LOGINOUT)
+    },
+    loginfo() {
+        return request(URLLIST.LOGINFO)
+    }
+
+
+}
---
+++ src/api/blog.js
@@ -0,0 +1,42 @@
+import request from '../helper/request.js'
+
+//所有接口
+//每个操作对应不同接口 
+const URLLIST = {
+    GET_LIST: '/blog',
+    GET_DETAIL: '/blog/:blogId',
+    CREATE: '/blog',
+    UPDATE: '/blog/:blogId',
+    DELETE: '/blog/:blogId'
+}//有些接口需要传参 ，指定具体的blog
+
+export default {
+
+    getBlogs({ page = 1, userId, atIndex } = { page: 1 }) {
+        return request(url, 'GET', {})
+    },
+
+    createBlog({ page = 1, userId, atIndex } = { page: 1 }) {
+        return request(url, 'GET', {})
+    },
+    updateBlog({ page = 1, userId, atIndex } = { page: 1 }) {
+        return request(url, 'GET', {})
+    },
+    deleteBlog({ page = 1, userId, atIndex } = { page: 1 }) {
+        return request(url, 'GET', {})
+    },
+    getIndexBlogs({ page = 1, userId, atIndex } = { page: 1 }) {
+        return request(url, 'GET', {})
+    },
+    getBlogsByUserId({ page = 1, userId, atIndex } = { page: 1 }) {
+        return request(url, 'GET', {})
+    },
+    getDetail() {
+        return request(url, 'PATCH', {})
+    }
+
+
+
+}
+
+//根据需求 设计接口和 不同功能块
