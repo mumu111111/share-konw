a2a84453fffbe743367f4415707f970b2a01a001
mumu111111 <2646709035@qq.com>
2019-09-16 17:38:19 +0800

    登录注册表单

---
+++ src/pages/Login/template.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+      <h4>用户名</h4>
+      <el-input placeholder="用户名"></el-input>
+      <p class="error">
+
+      </p>
+      <h4>密码</h4>
+      <el-input type="password" placeholder="密码"></el-input>
+      <p class="error">
+          
+      </p>
+      <el-button>立即登录</el-button>
+      <p>没有账户？<router-link to="/register">注册新用户</router-link></p>
+  </div>
+</template>
+
+<script src="./template.js"></script>
+
+<style lang="less" scoped src="./template.less"></style>
---
+++ src/pages/Register/template.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="register">
+      <h4>用户名</h4>
+      <el-input placeholder="用户名"></el-input>
+      <p class="error">
+
+      </p>
+      <h4>密码</h4>
+      <el-input type="password" placeholder="密码"></el-input>
+      <p class="error">
+          
+      </p>
+      
+      <h4>确认密码</h4>
+      <el-input type="password" placeholder="确认密码"></el-input>
+      <p class="error">
+          
+      </p>
+      <el-button>立即注册</el-button>
+      <p>已有账户？<router-link to="/login">直接登录</router-link></p>
+  </div>
+
+</template>
+
+<
+<script src="./template.js"></script>
+
+<style lang="less" scoped src="./template.less"></style>
